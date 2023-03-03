bd0b7db2565f41259981536b0515d671da0f8ef6
jeonghyeon Hwang <hijihilin@gmail.com>
2023-03-04 02:05:29 +0900

    [FIX] UIColor convenience init rgb 나누기 연산 추가 (#9)

--- CPR2U/CPR2U/UIColor+.swift
+++ CPR2U/CPR2U/UIColor+.swift
@@ -10,9 +10,10 @@ import UIKit
 extension UIColor {
     convenience init(rgb: Int) {
            self.init(
-            red: CGFloat((rgb >> 16) & 0xFF),
-            green: CGFloat((rgb >> 8) & 0xFF),
-            blue: CGFloat(rgb & 0xFF), alpha: 1
+            red: CGFloat((rgb >> 16) & 0xFF) / 255.0,
+            green: CGFloat((rgb >> 8) & 0xFF) / 255.0,
+            blue: CGFloat(rgb & 0xFF) / 255.0,
+            alpha: 1
            )
     }
     
