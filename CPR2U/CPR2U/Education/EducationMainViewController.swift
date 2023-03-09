54dc7538bab12e51e55ff887a3097c66ec68facd
jeonghyeon Hwang <hijihilin@gmail.com>
2023-03-09 19:16:11 +0900

    [FEAT] 교육 탭 메인 VC 구현 (#17)

---
+++ CPR2U/CPR2U/Education/EducationMainViewController.swift
@@ -0,0 +1,46 @@
+//
+//  EducationMainViewController.swift
+//  CPR2U
+//
+//  Created by 황정현 on 2023/03/09.
+//
+
+import UIKit
+
+final class EducationMainViewController: UIViewController {
+
+    private let certificateStatusView = CertificateStatusView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setUpConstraints()
+        setUpStyle()
+    }
+    
+    private func setUpConstraints() {
+        let safeArea = view.safeAreaLayoutGuide
+        let make = Constraints.shared
+        [
+            certificateStatusView
+        ].forEach({
+            view.addSubview($0)
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        })
+        
+        NSLayoutConstraint.activate([
+            certificateStatusView.topAnchor.constraint(equalTo: safeArea.topAnchor, constant: make.space16),
+            certificateStatusView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: make.space16),
+            certificateStatusView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor, constant: -make.space16),
+            certificateStatusView.heightAnchor.constraint(equalToConstant: 64)
+        ])
+    }
+    
+    private func setUpStyle() {
+        guard let navBar = self.navigationController?.navigationBar else { return }
+        navBar.prefersLargeTitles = true
+        navBar.topItem?.title = "Education"
+        navBar.largeTitleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.mainRed]
+        self.navigationController?.navigationItem.largeTitleDisplayMode = .automatic
+    }
+}
