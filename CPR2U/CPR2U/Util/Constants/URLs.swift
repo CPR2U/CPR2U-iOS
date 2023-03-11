5f573ecb869c62834a4e310b65163c3a4a625dcb
jeonghyeon Hwang <hijihilin@gmail.com>
2023-03-12 03:30:11 +0900

    [FEAT] Networking URL enum 추가 (#13)

---
+++ CPR2U/CPR2U/Util/Constants/URLs.swift
@@ -0,0 +1,16 @@
+//
+//  URLs.swift
+//  CPR2U
+//
+//  Created by 황정현 on 2023/03/11.
+//
+
+import Foundation
+
+enum URLs {
+    static let phoneNumberVertifyURL = "/auth/verification"
+    static let nicknameVertifyURL = "/auth/nickname"
+    static let signInURL = "/auth/login"
+    static let signUpURL = "/auth/signup"
+    static let autoLogin = "/auth/auto-login"
+}
