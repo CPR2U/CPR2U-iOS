0285990e56ac96853dc74c620a73838d08375f12
jeonghyeon Hwang <hijihilin@gmail.com>
2023-03-22 22:43:59 +0900

    [TEST] API 테스트용 환경 설정 (#32)

--- CPR2U/CPR2U/Scene/Splash/AutoLoginViewController.swift
+++ CPR2U/CPR2U/Scene/Splash/AutoLoginViewController.swift
@@ -46,7 +46,8 @@ final class AutoLoginViewController: UIViewController {
                 let result = try await viewModel.autoLogin()
                 usleep(1200000)
                 if result == true {
-                    let vc = TabBarViewController()
+//                    let vc = TabBarViewController()
+                    let vc = TestViewController()
                     guard let window = self.view.window else { return }
                     await window.setRootViewController(vc, animated: true)
                 } else {
