7c9d9fded22940ea494d8590e08bf95448cb0f90
jeonghyeon Hwang <hijihilin@gmail.com>
2023-03-26 22:22:43 +0900

    [FEAT] CallViewModel API 연결 메소드 구현 (#44)

--- CPR2U/CPR2U/Scene/Call/CallViewModel.swift
+++ CPR2U/CPR2U/Scene/Call/CallViewModel.swift
@@ -9,8 +9,13 @@ import Combine
 import Foundation
 
 final class CallViewModel: OutputOnlyViewModelType {
+    private var callManager: CallManager
     var timer: Timer.TimerPublisher?
     private let iscalled = CurrentValueSubject<Bool, Never>(false)
+    
+    init() {
+        callManager = CallManager(service: APIManager())
+    }
     
     struct Output {
         let isCalled: CurrentValueSubject<Bool, Never>
@@ -26,5 +31,31 @@ final class CallViewModel: OutputOnlyViewModelType {
     
     func cancelTimer() {
         timer?.connect().cancel()
+    }
+    
+    func receiveCallerList() async throws -> CallerListInfo? {
+        let result = Task { () -> CallerListInfo? in
+            let callResult = try await callManager.getCallerList()
+            
+            guard let list = callResult.data else { return nil }
+            return list
+        }
+        return try await result.value
+    }
+    
+    func callDispatcher(callerLocationInfo: CallerLocationInfo) async throws -> CallResult? {
+        let result = Task { () -> CallResult? in
+            let callResult = try await callManager.callDispatcher(callerLocationInfo: callerLocationInfo)
+            
+            guard let id = callResult.data else { return nil }
+            return id
+        }
+        return try await result.value
+    }
+    
+    func situationEnd(callId: Int) async throws {
+        Task {
+            try await callManager.situationEnd(callId: callId)
+        }
     }
 }
