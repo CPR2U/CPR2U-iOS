de2f821d9961ae86a7a7ef0351f242f3ed69efe7
jeonghyeon Hwang <hijihilin@gmail.com>
2023-05-24 22:47:20 +0900

    [FIX] 자세 연습 결과 화면 UI 수정 WIP (#82)

--- CPR2U/CPR2U/Scene/Education/Pose/PosePracticeResultViewController.swift
+++ CPR2U/CPR2U/Scene/Education/Pose/PosePracticeResultViewController.swift
@@ -10,6 +10,37 @@ import CombineCocoa
 import UIKit
 
 final class PosePracticeResultViewController: UIViewController {
+    
+    private let resultLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont(weight: .bold, size: 28)
+        label.textAlignment = .left
+        label.textColor = .mainWhite
+        label.text = "your_result_txt".localized()
+        return label
+    }()
+    
+    private lazy var scoreLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont(weight: .bold, size: 72)
+        label.textAlignment = .center
+        label.textColor = .mainWhite
+        label.text = "96/100" // TEST
+        label.attributedText = arrangeScoreText(text: label.text ?? "")
+        
+        return label
+    }()
+    
+    private lazy var scoreDescriptionLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont(weight: .bold, size: 24)
+        label.textAlignment = .left
+        label.numberOfLines = 2
+        label.textColor = .mainWhite
+        label.text = "Congratulations!\nYou passed the CPR posture test." // TEST
+        return label
+    }()
+    
     private let compressRateResultView: EvaluationResultView = {
         let view = EvaluationResultView()
         view.setImage(imgName: "ruler")
@@ -50,10 +81,10 @@ final class PosePracticeResultViewController: UIViewController {
     
     private let quitButton: UIButton = {
         let button = UIButton()
-        button.backgroundColor = .mainRed
-        button.layer.cornerRadius = 19
-        button.titleLabel?.font = UIFont(weight: .bold, size: 17)
-        button.setTitleColor(.mainWhite, for: .normal)
+        button.backgroundColor = .mainWhite
+        button.layer.cornerRadius = 24
+        button.titleLabel?.font = UIFont(weight: .bold, size: 20)
+        button.setTitleColor(.mainBlack, for: .normal)
         button.setTitle("quit".localized(), for: .normal)
         return button
     }()
@@ -79,18 +110,28 @@ final class PosePracticeResultViewController: UIViewController {
         setUpStyle()
         bind(viewModel: viewModel)
         setUpText()
+        
+        setUpOrientation(as: .landscape) // TEST CODE
     }
     
     private func setUpConstraints() {
         let safeArea = view.safeAreaLayoutGuide
         let make = Constraints.shared
         
+        let scoreStackView = UIStackView()
+        scoreStackView.axis  = NSLayoutConstraint.Axis.horizontal
+        scoreStackView.distribution  = UIStackView.Distribution.equalSpacing
+        scoreStackView.alignment = UIStackView.Alignment.center
+        scoreStackView.spacing   = 100
+        
         [
-            compressRateResultView,
-            pressDepthResultView,
-            handLocationResultView,
-            armAngleResultView,
-            finalResultView,
+            resultLabel,
+            scoreStackView,
+//            compressRateResultView,
+//            pressDepthResultView,
+//            handLocationResultView,
+//            armAngleResultView,
+//            finalResultView,
             quitButton
         ].forEach({
             view.addSubview($0)
@@ -105,43 +146,76 @@ final class PosePracticeResultViewController: UIViewController {
         })
         
         NSLayoutConstraint.activate([
-            compressRateResultView.topAnchor.constraint(equalTo: safeArea.topAnchor, constant: make.space24),
-            compressRateResultView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: make.space16)
+            resultLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: make.space16),
+            resultLabel.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor),
+            resultLabel.widthAnchor.constraint(equalToConstant: 200),
+            resultLabel.heightAnchor.constraint(equalToConstant: 36)
         ])
         
         NSLayoutConstraint.activate([
-            pressDepthResultView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor, constant: -make.space24),
-            pressDepthResultView.leadingAnchor.constraint(equalTo: compressRateResultView.leadingAnchor),
+            scoreStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            scoreStackView.topAnchor.constraint(equalTo: resultLabel.bottomAnchor),
+            scoreStackView.heightAnchor.constraint(equalToConstant: 108)
+        ])
+        
+        [
+            scoreLabel,
+            scoreDescriptionLabel
+        ].forEach({
+            scoreStackView.addSubview($0)
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        })
+        
+        NSLayoutConstraint.activate([
+            scoreLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 92),
+            scoreLabel.widthAnchor.constraint(equalToConstant: 158),
+            scoreLabel.heightAnchor.constraint(equalToConstant: 108)
         ])
         
         NSLayoutConstraint.activate([
-            handLocationResultView.topAnchor.constraint(equalTo: compressRateResultView.topAnchor),
-            handLocationResultView.leadingAnchor.constraint(equalTo: compressRateResultView.trailingAnchor, constant: make.space16)
+            scoreDescriptionLabel.leadingAnchor.constraint(equalTo: scoreLabel.trailingAnchor, constant: 100),
+            scoreDescriptionLabel.widthAnchor.constraint(equalToConstant: 396),
+            scoreDescriptionLabel.heightAnchor.constraint(equalToConstant: 72)
         ])
         
-        NSLayoutConstraint.activate([
-            armAngleResultView.bottomAnchor.constraint(equalTo: pressDepthResultView.bottomAnchor),
-            armAngleResultView.leadingAnchor.constraint(equalTo: compressRateResultView.trailingAnchor, constant: make.space16)
-        ])
+//        NSLayoutConstraint.activate([
+//            compressRateResultView.topAnchor.constraint(equalTo: safeArea.topAnchor, constant: make.space24),
+//            compressRateResultView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: make.space16)
+//        ])
+//
+//        NSLayoutConstraint.activate([
+//            pressDepthResultView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor, constant: -make.space24),
+//            pressDepthResultView.leadingAnchor.constraint(equalTo: compressRateResultView.leadingAnchor),
+//        ])
+//
+//        NSLayoutConstraint.activate([
+//            handLocationResultView.topAnchor.constraint(equalTo: compressRateResultView.topAnchor),
+//            handLocationResultView.leadingAnchor.constraint(equalTo: compressRateResultView.trailingAnchor, constant: make.space16)
+//        ])
+//
+//        NSLayoutConstraint.activate([
+//            armAngleResultView.bottomAnchor.constraint(equalTo: pressDepthResultView.bottomAnchor),
+//            armAngleResultView.leadingAnchor.constraint(equalTo: compressRateResultView.trailingAnchor, constant: make.space16)
+//        ])
         
         NSLayoutConstraint.activate([
-            quitButton.bottomAnchor.constraint(equalTo: armAngleResultView.bottomAnchor),
-            quitButton.leadingAnchor.constraint(equalTo: armAngleResultView.trailingAnchor, constant: make.space16),
-            quitButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -make.space16),
-            quitButton.heightAnchor.constraint(equalToConstant: 45)
+            quitButton.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor),
+            quitButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            quitButton.widthAnchor.constraint(equalToConstant: 206),
+            quitButton.heightAnchor.constraint(equalToConstant: 48)
         ])
         
-        NSLayoutConstraint.activate([
-            finalResultView.topAnchor.constraint(equalTo: handLocationResultView.topAnchor),
-            finalResultView.bottomAnchor.constraint(equalTo: quitButton.topAnchor, constant: -make.space12),
-            finalResultView.leadingAnchor.constraint(equalTo: quitButton.leadingAnchor),
-            finalResultView.trailingAnchor.constraint(equalTo: quitButton.trailingAnchor)
-            
-        ])
+//        NSLayoutConstraint.activate([
+//            finalResultView.topAnchor.constraint(equalTo: handLocationResultView.topAnchor),
+//            finalResultView.bottomAnchor.constraint(equalTo: quitButton.topAnchor, constant: -make.space12),
+//            finalResultView.leadingAnchor.constraint(equalTo: quitButton.leadingAnchor),
+//            finalResultView.trailingAnchor.constraint(equalTo: quitButton.trailingAnchor)
+//
+//        ])
     }
     
     private func setUpStyle() {
-        view.backgroundColor = .white
+        view.backgroundColor = .mainRed
     }
     
     private func bind(viewModel: EducationViewModel) {
@@ -165,5 +239,28 @@ final class PosePracticeResultViewController: UIViewController {
         pressDepthResultView.setDescriptionLabelText(as: result.pressDepth.description)
         score = result.compResult.score + result.angleResult.score + result.pressDepth.score + 1
         finalResultView.setUpScore(score: score)
+    }
+    
+    private func arrangeScoreText(text: String) -> NSAttributedString {
+        var range: Int = 0
+        let attributedString = NSMutableAttributedString(string: text)
+        if let rangeS = text.range(of: "/") {
+            range = text.distance(from: text.startIndex, to: rangeS.lowerBound)
+        }
+        
+        let realScoreAttributes: [NSAttributedString.Key: Any] = [
+            .font : UIFont(weight: .bold, size: 72) ?? UIFont(),
+            .foregroundColor : UIColor.mainWhite
+        ]
+        
+        let defaultScoreAttributes: [NSAttributedString.Key: Any] = [
+            .font : UIFont(weight: .bold, size: 32) ?? UIFont(),
+            .foregroundColor : UIColor.mainLightRed
+        ]
+        
+        attributedString.addAttributes(realScoreAttributes, range: NSRange(location: 0, length: range))
+        attributedString.addAttributes(defaultScoreAttributes, range: NSRange(location: range, length: attributedString.length - range))
+        
+        return attributedString
     }
 }
