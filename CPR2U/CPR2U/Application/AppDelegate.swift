f33b93922ee24b6a4593adef78eebd6cebe31741
jeonghyeon Hwang <hijihilin@gmail.com>
2023-05-28 20:52:40 +0900

    [STYLE] 주석 코드부 제거 (#78)

--- CPR2U/CPR2U/Application/AppDelegate.swift
+++ CPR2U/CPR2U/Application/AppDelegate.swift
@@ -56,11 +56,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
     
-//    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-//        let deviceTokenString = deviceToken.map { String(format: "%02x", $0) }.joined()
-//        Messaging.messaging().apnsToken = deviceToken
-//    }
-    
     func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
     }
 }
